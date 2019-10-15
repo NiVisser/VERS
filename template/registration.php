9ac5781684a77d51143f6600ae175e658ffa85bb
Nils Visser <nilsvisser1@gmail.com>
2019-10-15 12:08:16 +0200

    fixed sql / form statement

--- template/registration.php
+++ template/registration.php
@@ -5,8 +5,8 @@
 </head>
 <?php
 session_start();
-echo $_POST['name'];
-if(isset($_POST['name'])) {
+if(isset($_POST['submit'])) {
+   /*
     var_dump($_POST['date'] . ' ' . gettype($_POST['date']));
     $username = mysqli_real_escape_string($db, $_POST['name']);
     $email = mysqli_real_escape_string($db, $_POST['email']);
@@ -16,7 +16,8 @@ if(isset($_POST['name'])) {
     $user_check_query = "SELECT Username, Email FROM username WHERE Username='$username' OR Email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
-    echo '<h1>hallo</h1>';
+    
+
     if($user) { //user exists
         if($user['Username'] === $username or $user['Email'] === $email) {
             echo 'Gebruiker bestaat al.';
@@ -31,6 +32,21 @@ if(isset($_POST['name'])) {
 
         }
     }
+    */
+    $username = $_POST['name'];
+    $dob = $_POST['date'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user_add_query = 'INSERT INTO Username (Username, Password, isAmbassador, Email, Dateofbirth, Picture, Points) VALUES ("' . $username . '","' . $password . '",'. '"1","' . $email . '",' .  $dob . "," . '"test.jpg"' . "," .  '"1"' . ")";
+    //secho $user_add_query;
+    mysqli_query($db, $user_add_query);
+    if ($db->query($user_add_query) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $user_add_query . "<br>" . $db->error;
+}
+
 }
 ?>
 <body>
@@ -38,24 +54,24 @@ if(isset($_POST['name'])) {
     <div class="card-body">
         <h5 class="card-title">Vers</h5>
         <h6 class="card-subtitle mb-2 text-secondary">Stap 3/4</h6>
-        <form action="" method="post">
+        <form method="post">
             <div class="form-group">
                 <label for="name">Hoe heet je?</label>
-                <input class="form-control" id="name" placeholder="Wat is je naam?">
+                <input class="form-control" name="name" value="naam">
             </div>
 
             <div class="form-group">
                 <label for="date">Wanneer ben je jarig?</label>
-                <input type="date" class="form-control" id="date" placeholder="24-11-1995">
+                <input type="date" class="form-control" name="date" value="24-11-1995">
             </div>
 
             <div class="form-group">
                 <label for="email">Wat is je e-mailadres?</label>
-                <input type="email" class="form-control" id="email" placeholder="afra@forumvers.nl">
+                <input type="email" class="form-control" name="email" value="afra@forumvers.nl">
             </div>
             <div class="form-group">
                 <label for="password">Verzin een wachtwoord</label>
-                <input type="password" class="form-control" id="password" placeholder="Bijv. Fluitsnoepjes">
+                <input type="password" class="form-control" name="password" value="Fluitsnoepjes">
             </div>
             <button type="submit" name="submit" class="btn btn-primary">Submit</button>
         </form>
