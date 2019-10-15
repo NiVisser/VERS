c786bc145b604c3ccafda7f9a8d3e701fd2344d4
SJHelmhout <54943946+SJHelmhout@users.noreply.github.com>
2019-10-15 15:15:48 +0200

    intial webpage

--- template/config.php
+++ template/config.php
@@ -3,3 +3,8 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="style/master.css">
 <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"> 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+<link href="../style/css/master.css" rel="stylesheet">
