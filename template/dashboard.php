3a8ffd3958f5209abbd665dc3f2149a4ff07c827
unknown <pim.krips@gmail.com>
2019-10-16 03:02:46 +0200

    show data for ML

--- template/dashboard.php
+++ template/dashboard.php
@@ -35,13 +35,14 @@
         <h3> Test pagina </h3>
         <div class="wrapper3">
             <?php
-            $confidence = calculate_confidence($members, "Games", "Strips");
-            $advice = give_advice($members, $confidence, "Games", "Strips");
-            get_members($members);
+            $confidence = calculate_confidence($member_list, "games", "strips");
+            $advice = give_advice($member_list, $confidence, "games", "strips");
+            get_members($member_list);
             print("<br/>");
             print("member 4, maybe you would also like: " . $advice);
             print("<br/>");
             members_events($db, $users);
+//            print_r($member_list);
             ?>
         </div>
     </div>
